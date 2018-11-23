95f8548940b99f85f9d709b99ca3da9d1c314ca4
John Cruz <johnamcruz@gmail.com>
2018-11-23 11:47:32 -1000

    Initial Commit

---
+++ DaShuttleMap/DaShuttleMap.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace DaShuttleMap.Droid
+{
+    [Activity(Label = "DaShuttleMap", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ DaShuttleMap/DaShuttleMap/PageModels/MainPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FreshMvvm; 
+
+namespace DaShuttleMap.PageModels
+{
+    public class MainPageModel : FreshBasePageModel
+    {
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+        }
+    }
+}
